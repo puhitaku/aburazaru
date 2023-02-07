f62cb4147ddcc4109cb69f1dbdd4d28b542f1cb4
Takumi Sueda <puhitaku@gmail.com>
2023-02-07 12:33:54 +0900

    Improve Remove fbclid

--- remove_fbclid.js
+++ remove_fbclid.js
@@ -7,25 +7,7 @@
 // ==/UserScript==
 
 (function() {
-  // https://stackoverflow.com/questions/1634748/how-can-i-delete-a-query-string-parameter-in-javascript
-  function removeURLParameter(url, parameter) {
-    // prefer to use l.search if you have a location/link object
-    var urlparts = url.split('?');
-    if (urlparts.length >= 2) {
-      var prefix = encodeURIComponent(parameter) + '=';
-      var pars = urlparts[1].split(/[&;]/g);
-
-      // reverse iteration as may be destructive
-      for (var i = pars.length; i-- > 0;) {
-        // idiom for string.startsWith
-        if (pars[i].lastIndexOf(prefix, 0) !== -1) {
-          pars.splice(i, 1);
-        }
-      }
-      return urlparts[0] + (pars.length > 0 ? '?' + pars.join('&') : '');
-    }
-    return url;
-  }
-
-  window.history.pushState({}, "", removeURLParameter(window.location.href, 'fbclid'));
+  let url = new URL(window.location);
+  url.searchParams.delete('fbclid');
+  window.history.pushState({}, '', url.toString());
 })();
